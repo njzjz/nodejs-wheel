2580c039b6b75e195dc1400070c2895f2d6cc7b7
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-10-03 00:07:49 -0400

    build win64 (#2)

--- nodejs_wheel/executable.py
+++ nodejs_wheel/executable.py
@@ -8,7 +8,8 @@ ROOT_DIR = os.path.dirname(__file__)
 
 
 def _program(name, args):
-    return subprocess.call([os.path.join(ROOT_DIR, "bin", name)] + args, close_fds=False)
+    bin_dir = ROOT_DIR if os.name == 'nt' else os.path.join(ROOT_DIR, "bin")
+    return subprocess.call([os.path.join(bin_dir, name)] + args, close_fds=False)
 
 
 def node():
