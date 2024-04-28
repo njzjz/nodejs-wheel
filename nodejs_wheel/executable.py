9738bc65160911b0a87154b5e4afbeb536be6289
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-04-28 11:33:42 -0400

    add Python API (#26)

--- nodejs_wheel/executable.py
+++ nodejs_wheel/executable.py
@@ -7,29 +7,86 @@ import sys
 ROOT_DIR = os.path.dirname(__file__)
 
 
-def _program(name, args):
+def _program(name, args, **kwargs):
     bin_dir = ROOT_DIR if os.name == 'nt' else os.path.join(ROOT_DIR, "bin")
-    return subprocess.call([os.path.join(bin_dir, name)] + args, close_fds=False)
+    return subprocess.call([os.path.join(bin_dir, name)] + args, **kwargs)
 
 
-def call_node(*args):
+def call_node(*args, **kwargs):
     suffix = '.exe' if os.name == 'nt' else ''
-    raise SystemExit(_program('node' + suffix, list(args)))
+    return _program('node' + suffix, list(args), **kwargs)
 
 
-def node(args=None):
+def node(args=None, **kwargs):
+    """Call the node executable with the given arguments.
+
+    Parameters
+    ----------
+    args : Optional[list[str]], default=None
+        List of arguments to pass to the node executable.
+        If None, the arguments are taken from sys.argv[1:].
+    **kwargs : dict[str, Any]
+        Other arguments passed to subprocess.call
+
+    Returns
+    -------
+    int
+        Return code of the node executable.
+    """
     if args is None:
         args = sys.argv[1:]
-    call_node(*args)
+    return call_node(*args, **kwargs)
 
 
-def npm(args=None):
+def npm(args=None, **kwargs):
+    """Call the npm executable with the given arguments.
+
+    Parameters
+    ----------
+    args : Optional[list[str]], default=None
+        List of arguments to pass to the npm executable.
+        If None, the arguments are taken from sys.argv[1:].
+    **kwargs : dict[str, Any]
+        Other arguments passed to subprocess.call
+
+    Returns
+    -------
+    int
+        Return code of the npm executable.
+    """
     if args is None:
         args = sys.argv[1:]
-    call_node(os.path.join(ROOT_DIR, "lib", "node_modules", "npm", "bin", "npm-cli.js"), *args)
+    return call_node(os.path.join(ROOT_DIR, "lib", "node_modules", "npm", "bin", "npm-cli.js"), *args, **kwargs)
 
 
-def npx(args=None):
+def npx(args=None, **kwargs):
+    """Call the npx executable with the given arguments.
+
+    Parameters
+    ----------
+    args : Optional[list[str]], default=None
+        List of arguments to pass to the npx executable.
+        If None, the arguments are taken from sys.argv[1:].
+    **kwargs : dict[str, Any]
+        Other arguments passed to subprocess.call
+
+    Returns
+    -------
+    int
+        Return code of the npx executable.
+    """
     if args is None:
         args = sys.argv[1:]
-    call_node(os.path.join(ROOT_DIR, "lib", "node_modules", "npm", "bin", "npx-cli.js"), *args)
+    return call_node(os.path.join(ROOT_DIR, "lib", "node_modules", "npm", "bin", "npx-cli.js"), *args, **kwargs)
+
+
+def _node_entry_point():
+    raise SystemExit(node(close_fds=False))
+
+
+def _npm_entry_point():
+    raise SystemExit(npm(close_fds=False))
+
+
+def _npx_entry_point():
+    raise SystemExit(npx(close_fds=False))
---
+++ tests/test_api.py
@@ -0,0 +1,9 @@
+from nodejs_wheel import (
+    node,
+    npm,
+    npx,
+)
+
+node(["--version"])
+npm(["--version"])
+npx(["--version"])
