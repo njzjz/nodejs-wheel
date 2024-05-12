e55a1000e79f04072bfdc562946435ed3c667224
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-05-12 03:32:31 -0400

    feat: add an argument to return CompletedProcess
    Fix #34.
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,6 +6,17 @@ from nodejs_wheel import (
     npx,
 )
 
-node(["--version"])
-npm(["--version"])
-npx(["--version"])
+return_code0 = node(["--version"])
+return_code1 = npm(["--version"])
+return_code2 = npx(["--version"])
+assert return_code0 == 0
+assert return_code1 == 0
+assert return_code2 == 0
+
+completed_process0 = node(["--version"], return_completed_process=True)
+completed_process1 = npm(["--version"], return_completed_process=True)
+completed_process2 = npx(["--version"], return_completed_process=True)
+
+assert completed_process0.returncode == 0
+assert completed_process1.returncode == 0
+assert completed_process2.returncode == 0
