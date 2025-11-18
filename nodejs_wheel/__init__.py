9ddcd1bebfeeb7985f5dc4c16d3680a61680ea6b
Jinzhe Zeng <jinzhe.zeng@ustc.edu.cn>
2025-11-18 23:13:27 +0800

    remove corepack

--- nodejs_wheel/__init__.py
+++ nodejs_wheel/__init__.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
 
 from .executable import (
-    corepack,
     node,
     npm,
     npx,
 )
 
 __all__ = [
-    "corepack",
     "node",
     "npm",
     "npx",
